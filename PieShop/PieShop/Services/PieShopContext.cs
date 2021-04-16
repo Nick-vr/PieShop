db021730cd61d1426e315e2943e213c462c1fc5a
Nick-vr <nvanroyen@gmail.com>
2021-04-16 14:26:11 +0200

    Add sqlite db

---
+++ PieShop/PieShop/Services/PieShopContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using PieShop.Models;
+using Xamarin.Essentials;
+
+namespace PieShop.Services
+{
+    public sealed class PieShopContext : DbContext
+    {
+        public DbSet<Gum> Gums { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+
+        public PieShopContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "PieShop.sqlite");
+            optionsBuilder.UseSqlite($"Filename = {dbPath}");
+        }
+    }
+}
